3ac84b792565bf073464fadf8a293b25c8012bfe
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2022-11-04 14:52:08 +0000

    Compleated form

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+
 namespace RazorInputForm.Pages
 {
     public class IndexModel : PageModel
     {
+
+        [BindProperty]
+        public string FullName { get; set; }
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -14,6 +18,20 @@ namespace RazorInputForm.Pages
 
         public void OnGet()
         {
+            FullName = "Sudath Nawagamuwage";
+        }
+        public void OnPost()
+        {
+            if(String.IsNullOrWhiteSpace(FullName))
+            {
+                ViewData["Message"] = "Name is missing!";
+                FullName = "Please Fill The Details";
+            }
+            else
+            {
+                //Register the user
+                ViewData["Message"] = "Name is Registered!";
+            }
 
         }
     }
